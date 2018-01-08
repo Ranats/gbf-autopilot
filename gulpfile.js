4e38c9636cbfe14d1d33ea51bdcbe7a9c7f88421
Izra Faturrahman <Frizz925@hotmail.com>
2018-01-09 00:49:52 +0700

    Removed gutil. Updated travis configuration.

--- gulpfile.js
+++ gulpfile.js
@@ -111,7 +111,7 @@ gulp.task("serve2", shell.task([
 ]));
 
 gulp.task("config", function() {
-  return gulp.src("./{server,controller,extension,.}/*.sample.{json,ini}")
+  return gulp.src("./{extension,.}/*.sample.{json,ini}")
     .pipe(rename(function(path) {
       const suffix = ".sample";
       const index = path.basename.length - suffix.length;
