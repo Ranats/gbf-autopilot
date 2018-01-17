93a3727f8ed2eb5c0723d3c8fa853f533ab9139a
Izra Faturrahman <Frizz925@hotmail.com>
2018-01-17 17:45:30 +0700

    Guild War update

--- test/DryRun.js
+++ test/DryRun.js
@@ -1,7 +1,8 @@
 const serverRunner = require("../server");
 
 describe("Test server dry run", () => {
-  it("should listen normally", (done) => {
+  it("should listen normally", function(done) {
+    this.timeout(5000);
     serverRunner().then((server) => {
       server.close(done);
     });
