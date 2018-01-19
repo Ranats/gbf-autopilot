f12772714e6c76b79d0c441f939d10e4cf5e62b4
Izra Faturrahman <Frizz925@hotmail.com>
2018-01-19 22:54:32 +0700

    Set the test case timeout limit to 10 seconds

--- test/DryRun.js
+++ test/DryRun.js
@@ -2,7 +2,7 @@ const serverRunner = require("../server");
 
 describe("Test server dry run", () => {
   it("should listen normally", function(done) {
-    this.timeout(5000);
+    this.timeout(10000);
     serverRunner().then((server) => {
       server.close(done);
     });
