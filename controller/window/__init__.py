e3a65b047b2443dc72d059eed3540249759df172
Izra Faturrahman <Frizz925@hotmail.com>
2018-02-02 02:14:35 +0700

    Respect the dummy window env regardless of the platform

--- controller/window/__init__.py
+++ controller/window/__init__.py
@@ -3,10 +3,10 @@ import os
 from .abstract_window import AbstractWindow
 from .dummy_window import DummyWindow
 
-if sys.platform == 'win32':
-    from .win32_window import Win32Window as Window
+if os.environ['GBF_AUTOPILOT_USE_DUMMY_WINDOW']:
+    Window = DummyWindow
 else:
-    if os.environ['GBF_AUTOPILOT_USE_DUMMY_WINDOW']:
-        Window = DummyWindow
+    if sys.platform == 'win32':
+        from .win32_window import Win32Window as Window
     else:
         raise OSError("Platform '%s' is not supported!" % sys.platform)
