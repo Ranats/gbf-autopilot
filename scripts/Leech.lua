3f26d598c28388313ddde0dbac30940f627cbe9b
Ranats <sopurani885@gmail.com>
2018-06-11 10:01:54 +0900

    add xeno

--- scripts/Leech.lua
+++ scripts/Leech.lua
@@ -1,14 +1,18 @@
-character_1:UseSkill(2)
+character_1:UseSkill(4)
     :UseSkill(3)
-    :UseSkill(4)
---character_2:UseSkill(1)
+    :UseSkill(1)
+character_2:UseSkill(2)
 --    :UseSkill(2)
 --    :UseSkill(3)
-character_3:UseSkill(1)
---    :UseSkill(2)
 character_4:UseSkill(2)
+--if turn > 3 then
+    character_4:UseSkill(1)
+--end
+
+character_3:UseSkill(2)
 --    :UseSkill(2)
 --    :UseSkill(3)
+character_2:UseSkill(1)
 
 --Summon(6)
 
